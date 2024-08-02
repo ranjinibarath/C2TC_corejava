916bae8de5db6d75aff867978a89d63fa9cb0244
ranjinibarath <ranjinibarath69@gmail.com>
2024-08-02 01:12:28 -0700

    Method Overloading program

---
+++ Firstproject/Firstproject/src/com/tns/day7/methodoverloading/MethodOverloading.java
@@ -0,0 +1,29 @@
+package com.tns.day7.methodoverloading;
+
+public class MethodOverloading {
+	
+
+		public static int addition(int a, int b) {
+			return a + b;
+		}
+
+		public static float addition(float a, float b) {
+			return a + b;
+		}
+
+		public static float addition(int a, float b) {
+			return a + b;
+		}
+
+		public static float addition(float a, int b) {
+			return a + b;
+		}
+
+		public static String addition(String a, String b) {
+			return a + b;
+		}
+
+	}
+
+
+
---
+++ Firstproject/Firstproject/src/com/tns/day7/methodoverloading/Point.java
@@ -0,0 +1,32 @@
+package com.tns.day7.methodoverloading;
+
+public class Point {
+	//Program to demonstrate constructor overloading - Compile Time Polymorphism
+	
+		private float x;
+		private float y;
+
+		public Point() {
+			x = 0.0f;
+			y = 0.0f;
+		}
+
+		public Point(float x) {
+			this.x = x;
+			this.y = x;
+		}
+
+		public Point(float x, float y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public String toString() {
+			return "Point [x=" + x + ", y=" + y + "]";
+		}
+
+	}
+
+
+
