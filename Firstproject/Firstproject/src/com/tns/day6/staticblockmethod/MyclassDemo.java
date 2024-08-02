0f9b22b13936d79d4ae006090c26e58d4af8ff6d
ranjinibarath <ranjinibarath69@gmail.com>
2024-08-01 20:08:26 -0700

    Static method program

---
+++ Firstproject/Firstproject/src/com/tns/day6/staticblockmethod/MyclassDemo.java
@@ -0,0 +1,27 @@
+package com.tns.day6.staticblockmethod;
+
+public class MyclassDemo {
+
+	public static void main(String[] args) {
+	
+				System.out.println("First Line");
+				MyClass o1 = new MyClass();
+				System.out.println(o1);
+
+				// static method with class name
+				MyClass.display();
+
+				MyClass o2 = new MyClass();
+				System.out.println(o2);
+				MyClass.display();
+				MyClass o3 = new MyClass();
+				System.out.println(o3);
+				MyClass.display();
+
+			}
+
+		}
+
+
+	
+
