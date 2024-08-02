f164d062452f24091ee0c24599a292f3fdbb9e67
ranjinibarath <ranjinibarath69@gmail.com>
2024-08-02 01:39:45 -0700

    Array program

---
+++ Firstproject/Firstproject/src/com/tns/multidimension/ArrayDemo.java
@@ -0,0 +1,17 @@
+package com.tns.multidimension;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int a[][];
+		int i,j;
+		
+		for(i=1;i<=3;i++) {
+			for(j=1;j<=3;j++) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
